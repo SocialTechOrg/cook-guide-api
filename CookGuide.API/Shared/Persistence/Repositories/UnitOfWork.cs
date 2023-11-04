93027cda77746fd47f6ff566e707dd50fa160326
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-11-04 03:17:16 -0500

    add: Unit of work
    Co-Authored-By: L.T. <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>

---
+++ CookGuide.API/Shared/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using CookGuide.API.Shared.Domain.Repositories;
+using CookGuide.API.Shared.Persistence.Contexts;
+
+namespace CookGuide.API.Shared.Persistence.Repositories;
+
+public class UnitOfWork: IUnitOfWork
+{
+
+    private readonly AppDbContext context;
+    
+    public UnitOfWork(AppDbContext context)
+    {
+        this.context = context;
+    }
+    
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
