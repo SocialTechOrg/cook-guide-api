e11b9909dbbdc8007842dc6d5cc1a1470aa55d2a
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-11-05 09:05:32 -0500

    hotfix: account entity

--- CookGuide.API/Accounts/Domain/Models/Accounts.cs
+++ CookGuide.API/Accounts/Domain/Models/Accounts.cs
@@ -10,7 +10,7 @@ public class Accounts
     public bool? userType { get; set; }
     public string username { get; set; }
     public string password { get; set; }
-    public string? email { get; set; }
+    public string email { get; set; }
     public string? phone { get; set; }
     public string? address { get; set; }
     
