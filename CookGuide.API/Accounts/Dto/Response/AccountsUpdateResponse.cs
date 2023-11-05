aede3cb610ed6448743a0218de92102e64efea4f
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-11-05 09:11:25 -0500

    hotfix: updated response DTO

--- CookGuide.API/Accounts/Dto/Response/AccountsUpdateResponse.cs
+++ CookGuide.API/Accounts/Dto/Response/AccountsUpdateResponse.cs
@@ -5,7 +5,6 @@ public class AccountsUpdateResponse
     public string? firstName { get; set; }
     public string? lastName { get; set; }
     public string? username { get; set; }
-    public string? password { get; set; }
     public string? email { get; set; }
     public string? phone { get; set; }
     public string? address { get; set; }
