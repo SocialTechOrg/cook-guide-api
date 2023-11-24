1b56c3fc4d7b194e5457f4e8ad884a6f6cd4f110
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-11-23 23:01:12 -0500

    Add: Ingredients service
    Co-Authored-By: L.T. <104176051+NotLT42@users.noreply.github.com>

---
+++ CookGuide.API/Ingredients/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace CookGuide.API.Ingredients.Mapping;
+
+using CookGuide.API.Ingredients.Dto.Request;
+using CookGuide.API.Ingredients.Domain.Models;
+
+public class ResourceToModelProfile: Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<IngredientsAddRequest, Ingredients>();
+        CreateMap<IngredientsUpdateRequest, Ingredients>();
+    }
+}
--- CookGuide.API/Program.cs
+++ CookGuide.API/Program.cs
@@ -1,11 +1,23 @@
 using CookGuide.API.Accounts.Domain.Repositories;
 using CookGuide.API.Accounts.Domain.Services;
-using CookGuide.API.Accounts.Mapping;
 using CookGuide.API.Accounts.Services;
+using CookGuide.API.Ingredients.Domain.Models;
+using CookGuide.API.Ingredients.Domain.Services;
+using CookGuide.API.Ingredients.Services;
+using CookGuide.API.Recipes.Domain.Models;
+using CookGuide.API.Recipes.Domain.Services;
+using CookGuide.API.Recipes.Services;
 using CookGuide.API.Shared.Domain.Repositories;
 using CookGuide.API.Shared.Persistence.Contexts;
 using CookGuide.API.Shared.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using ModelToResourceProfileAccounts = CookGuide.API.Accounts.Mapping.ModelToResourceProfile;
+using ResourceToModelProfileAccounts  = CookGuide.API.Accounts.Mapping.ResourceToModelProfile;
+using ModelToResourceProfileIngredients = CookGuide.API.Ingredients.Mapping.ModelToResourceProfile;
+using ResourceToModelProfileIngredients  = CookGuide.API.Ingredients.Mapping.ResourceToModelProfile;
+using ModelToResourceProfileRecipes = CookGuide.API.Recipes.Mapping.ModelToResourceProfile;
+using ResourceToModelProfileRecipes  = CookGuide.API.Recipes.Mapping.ResourceToModelProfile;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,15 +39,28 @@ builder.Services.AddDbContext<AppDbContext>(
 
 builder.Services.AddScoped<IAccountsService, AccountsService>();
 builder.Services.AddScoped<IAccountsRepository, AccountsRepository>();
+builder.Services.AddScoped<IIngredientsRepository, IngredientsRepository>();
+builder.Services.AddScoped<IIngredientsService, IngredientsService>();
+builder.Services.AddScoped<IRecipesRepository, RecipesRepository>();
+builder.Services.AddScoped<IRecipesIngredientsRepository, RecipesIngredientsRepository>();
+builder.Services.AddScoped<IRecipesService, RecipesService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
- 
+
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddAutoMapper(
-    typeof(ModelToResourceProfile), 
-    typeof(ResourceToModelProfile)
+    //Accounts
+    typeof(ModelToResourceProfileAccounts), 
+    typeof(ResourceToModelProfileAccounts),
+    //Ingredients
+    typeof(ModelToResourceProfileIngredients),
+    typeof(ResourceToModelProfileIngredients),
+    //Recipes
+    typeof(ModelToResourceProfileRecipes),
+    typeof(ResourceToModelProfileRecipes)
 );
+
 
 var app = builder.Build();
 
