ce326068030a861a098665cbcd1b5c50af2e632e
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-11-23 23:12:12 -0500

    Add: mappers
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
    Co-Authored-By: William062004 <89233287+William062004@users.noreply.github.com>
    Co-Authored-By: L.T. <104176051+NotLT42@users.noreply.github.com>

---
+++ CookGuide.API/Recipes/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CookGuide.API.Recipes.Domain.Models;
+using CookGuide.API.Recipes.Dto.Response;
+
+namespace CookGuide.API.Recipes.Mapping;
+
+public class ModelToResourceProfile: Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Domain.Models.Recipes, Dto.Response.RecipeUpdateResponse>();
+
+        CreateMap<Domain.Models.Recipes, RecipeAddResponse>()
+            .ForMember(dest => dest.recipeId, opt => opt.MapFrom(src => src.id))
+            .ForMember(dest => dest.description, opt => opt.MapFrom(src => src.description))
+            .ForMember(dest => dest.num_portions, opt => opt.MapFrom(src => src.num_portions))
+            .ForMember(dest => dest.ingredients, opt => opt.MapFrom(src => src.ingredients));
+        
+        CreateMap<Domain.Models.Recipes, Dto.Response.RecipeDeleteResponse>();
+        CreateMap<Accounts.Domain.Models.Accounts, Dto.Response.AccountsRecipeResponse>();
+        
+        CreateMap<RecipesIngredients, IngredientRecipeResponse>()
+            .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.IngredientId))
+            .ForMember(dest => dest.quantity, opt => opt.MapFrom(src => src.Quantity))
+            .ForMember(dest => dest.measurement, opt => opt.MapFrom(src => src.Measurement));
+    }
+}
