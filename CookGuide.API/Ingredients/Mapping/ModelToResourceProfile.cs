9d36097b2a39139b8a74cb68ed6ad1f3dc8a3ff1
Lucía A <91093842+LuciaAliagaT@users.noreply.github.com>
2023-11-23 23:17:37 -0500

    Add: Model to resource
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
    Co-Authored-By: William062004 <89233287+William062004@users.noreply.github.com>
    Co-Authored-By: L.T. <104176051+NotLT42@users.noreply.github.com>

---
+++ CookGuide.API/Ingredients/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CookGuide.API.Ingredients.Dto.Response;
+
+namespace CookGuide.API.Ingredients.Mapping;
+using CookGuide.API.Ingredients.Domain.Models;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+
+        CreateMap<Ingredients, IngredientsUpdateResponse>();
+        CreateMap<Ingredients, IngredientsAddResponse>();
+        CreateMap<Ingredients, IngredientsDeleteResponse>();
+    }
+}
